68e9dedec3eddbb8e3836693d43abff56fe5c196
wagwbot <76049451+wagwbot@users.noreply.github.com>
2020-12-17 14:21:57 +0700

    lib

---
+++ info.js
@@ -0,0 +1,30 @@
+exports.info = (id, XPTN, corohelp, tampilTanggal, tampilWaktu, instagram, nomer, aktif, groupwa, youtube) => {
+	return `*MENU ${XPTN}*
+  
+❉──────────❉
+  Hi. *${id.split("@s.whatsapp.net")[0]}* 👋️
+❉──────────❉
+
+         ───
+📆 *${tampilTanggal}*
+⏱️ *${tampilWaktu}*
+📢 Bot Aktif ; *${aktif}*
+         ───
+         
+╔════════════════════
+║ *About ${RAMA}
+╠════════════════════
+║├≽️⚜ *🔰AUTHOR🔰*: LEON
+║├≽️🛡️ *DESIGNER*: RAMA
+╠════════════════════
+══════════════
+║  *${RAMA}*
+╠════════════════════
+║╭──❉ *SOSMED ADMIN* ❉─*
+║│2. *Creator ${XPTN}*
+║│ _${0895355566000}_
+║╰───────────
+╠════════════════════
+║ *MADE BY RAMA*
+╚════════════════════`
+}
---
+++ xp.js
@@ -0,0 +1,167 @@
+exports.xp = (id, XPTN, corohelp, tampilTanggal, tampilWaktu, instagram, nomer, aktif, groupwa, youtube) => {
+	return `
+  
+❉──────────❉
+  Hi. *${id.split("@s.whatsapp.net")[0]}* 👋️
+  
+❉──────────❉
+         ───
+*𝔹𝕆𝕋 𝕐𝕋*
+
+*User*       :*${id.split("@s.whatsapp.net")[0]}*
+*Status*     : JOMBLO
+*Rank*	     :MEMBER
+*REGION*   :ID
+*SC*         :JS (termux) 
+*FITUR       :106
+
+*Total Pengguna Bot*: 
+ ⚕️Group :27
+ ⚕️User : 18
+
+╔══✪〘  𝙈𝙀𝙈𝘽𝙐𝘼𝙏  〙✪══
+║
+╠➥ 🔥sticker [foto]
+╠➥ 🔥memecreate
+╠➥ 🔥nulis
+╠➥ 🔥nulis2
+╠➥ 🔥gltext <new>
+╠➥ 🔥text3d [3dtext] <new>
+║
+╠══✪〘 𝘼𝘿𝙈𝙄𝙉 〙✪══
+║
+╠➥ .closegc [tutupgrub] <new>
+╠➥ .opengc [bukagrub] <new>
+╠➥ .setname [ubhnamagrub]
+╠➥ .setdesc [ubhdes] <new>
+╠➥ .tagme [tag] <new>
+║
+╠══✪〘 𝙈𝙀𝘿𝙄𝘼 〙✪══
+║
+╠➥ .yt [link]
+╠➥ .ytmp4 [link]
+╠➥ .ytmp3 [link]
+╠➥ .twt [link]
+╠➥ .fb [link]
+╠➥ .tiktok [link]
+╠➥ .ig [link]
+╠➥ .cocofun [link]
+╠➥ .joox [link]
+║
+╠══✪〘 𝙎𝙊𝙎𝙄𝘼𝙇 〙✪══
+║
+╠➥ .cersex [fix]
+╠➥ .cersex2 [fix]
+╠➥ .indohot
+╠➥ .pantun1
+╠➥ .pantun2
+╠➥ .puisi1
+╠➥ .puisi2
+╠➥ .cerpen
+╠➥ .quotes1
+╠➥ .quotes2
+╠➥ .katabijak
+╠➥ .quran
+║
+╠══✪〘 𝙂𝙍𝙊𝙐𝙋 〙✪══
+║
+╠➥ .nulis [text]
+╠➥ ! [text] <simi>
+╠➥ .alay
+║
+╠══✪〘 𝙍𝘼𝙈𝘼𝙇𝘼𝙉 〙✪══ 
+║
+╠➥ .fakta [fakta]
+╠➥ .zodiak [16-02-2003]
+╠➥ .nama [namalu]
+╠➥ .pasangan [L & C]
+╠➥ .namaninja [namalu]
+╠➥ .namajenis [jenis]
+║
+╠══✪〘 𝙒𝙄𝘽𝙐 〙✪══
+║
+╠➥ .nekopoi [fix]
+╠➥ .waifu [wibu]
+╠➥ .infoanime [judul]
+╠➥ .shota [wibu]
+╠➥ .loli [animekecil]
+╠➥ .nekoanime 
+╠➥ .hentai 
+╠➥ .filmanime [judul]
+╠➥ .dewabatch [anime]
+╠➥ .pokemon [img]
+╠➥ .quotesanime [random]
+║
+╠══✪〘 𝙄𝙉𝙁𝙊 〙✪══
+║
+╠➥ .owner [pembuat]
+╠➥ .freadsowner [temen]
+╠➥ .coowner [wakil]
+╠➥ .donasi [sedekah]
+╠➥ .github [sc]
+╠➥ .groub [groub]
+╠➥ .ping [speed]
+╠➥ .infoig [nickname]
+╠➥ .profileig [link]
+║
+╠══✪〘 𝙐𝙈𝙐𝙈 〙✪══
+║
+╠➥ .foto cewek
+╠➥ .foto cowok
+╠➥ .bitly [link]
+╠➥ .tts 
+╠➥ .ttp
+╠➥ .resep [makanan]
+╠➥ .textimage [ssimg]
+╠➥ .chord [gitar]
+╠➥ .lirik [judul]
+╠➥ .hotsearch 
+╠➥ .map [daerah]
+╠➥ .pesankosong
+║
+╠══✪〘 𝙎𝙀𝙇𝙀𝘽𝙄𝙃𝙉𝙔𝘼 〙✪══
+║
+╠➥ 🔥ssweb [link]
+╠➥ 🔥meme [inggris]
+╠➥ 🔥randomhentai
+╠➥ 🔥covid [jumlahcovid]
+╠➥ 🔥suhu [kondisi]
+╠➥ 🔥batrai [batraibot]
+╠➥ 🔥spamcall [816...]
+╠➥ 🔥spamsms [816...]
+╠➥ 🔥spamgmail [gmail]
+╠➥ 🔥cooltext [text]
+╠➥ 🔥jadwalTVnow [sekarang]
+╠➥ 🔥jadwalbola [sekarang]
+╠➥ 🔥tanggal [sekarang]
+╠➥ 🔥waktu [sekarang]
+╠➥ 🔥coronainfoall [all]
+╠➥ 🔥harinasional [17 agus...]
+╠➥ 🔥bucin
+╠➥ 🔥icon
+╠➥ 🔥animesearch 
+╠➥ 🔥ocr [linkweb]
+╠➥ 🔥infogempa
+╠➥ 🔥blackpink [text]
+╠➥ 🔥texthunder [text]
+╠➥ 🔥tts2 
+╠➥ 🔥quotemarker
+╠➥ 🔥gaming2 [text]
+╠➥ 🔥randomkis [linkweb]
+╠➥ 🔥urltoimg [linkweb]
+╠➥ 🔥joox [text]
+╠➥ 🔥nulis2 [text]
+╠➥ 🔥randomhemtai2
+╠➥ 🔥createtext
+╠➥ 🔥randomcry
+║
+╚═〘  𝔹𝕆𝕋 〙✪══
+
+       ║▌│█║▌│ █║▌│█│║▌║
+       ║▌│█║▌│ █║▌│█│║▌║
+
+             🔥RAMA*
+╰➤
+    ╰➤𝐒𝐔𝐁𝐒𝐂𝐑𝐈𝐁𝐄 : RISKA GAMING
+}
+ 
